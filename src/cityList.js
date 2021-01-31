dda1c74a32267e3ce55eb1c2be37f5a2b4f24c84
ankita <ankitaa@nova.tech>
2021-02-01 02:49:01 +0530

    ui

---
+++ src/cityList.js
@@ -0,0 +1,84 @@
+const cityList = [
+    {
+        id: 1,
+        name: 'Khartoum',
+        desc: 'Aenean lacinia bibendum nulla sed consectetur.',
+        image: 'https://marvel-live.freetls.fastly.net/canvas/2020/9/af686165ae8d48fe9fc6f6368255688a?quality=95&fake=.png',
+        tags: [
+            'Wellington',
+            'Tianjin'
+        ]
+    },
+    {
+        id: 2,
+        name: 'Santa Clara',
+        desc: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur et.',
+        image: 'https://marvel-live.freetls.fastly.net/canvas/2020/9/084a450bf1da47799449031d95a59bba?quality=95&fake=.png',
+        tags: [
+            'Salt Lake City',
+            'Seville'
+        ]
+    },
+    {
+        id: 3,
+        name: 'Bengaluru',
+        desc: 'Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+        image: 'https://marvel-live.freetls.fastly.net/canvas/2020/9/b4da41474c0640cfa0695f68cd7b353c?quality=95&fake=.png',
+        tags: [
+            'Puebla',
+        ]
+    },
+    {
+        id: 4,
+        name: 'Portland',
+        desc: 'Tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+        image: 'https://marvel-live.freetls.fastly.net/canvas/2020/9/95de1ebd71ff407b9b6a42d22cdafe10?quality=95&fake=.png',
+        tags: [
+            'Sacramento',
+            'Osaka',
+            'Astana'
+        ]
+    },
+    {
+        id: 5,
+        name: 'Fairbanks',
+        desc: 'Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+        image: 'https://marvel-live.freetls.fastly.net/canvas/2020/9/3a472ea6709a4eceb3e8bc385220d71a?quality=95&fake=.png',
+        tags: [
+            'Belgrade',
+            'Cali',
+            'Stockholm'
+        ]
+    },
+    {
+        id: 6,
+        name: 'Sofia',
+        desc: 'Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+        image: 'https://marvel-live.freetls.fastly.net/canvas/2020/9/e891e109877d4cbfb4d3e070e825eff6?quality=95&fake=.png',
+        tags: [
+            'Trondheim',
+            'Xian'
+        ]
+    },
+    {
+        id: 7,
+        name: 'Belfast',
+        desc: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla.',
+        image: 'https://marvel-live.freetls.fastly.net/canvas/2020/9/9b1e75bcc09949b0acdb1c14e3cfa9b0?quality=95&fake=.png',
+        tags: [
+            'Aarhus',
+            'Ankara'
+        ]
+    },
+    {
+        id: 8,
+        name: 'Pune',
+        desc: 'Mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+        image: 'https://marvel-live.freetls.fastly.net/canvas/2020/9/92044c9b6ad54503b5faf6e278b7b22a?quality=95&fake=.png',
+        tags: [
+            'Sylhet',
+        ]
+    },
+]
+
+export default cityList;
